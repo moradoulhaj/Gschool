c88998408a83539017c9d2328cf57e6f44029c2f
moradoulhaj <moradoulhaj.off@gmail.com>
2025-02-28 01:21:17 +0100

    Adding the entities

---
+++ src/main/java/com/gschool/entities/Filiere.java
@@ -0,0 +1,54 @@
+package com.gschool.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "filiere")
+public class Filiere {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "code", nullable = false, length = 50)
+    private String code;
+
+    @Column(name = "libelle", nullable = false, length = 100)
+    private String libelle;
+
+    @Column(name = "nbr_etudiant")
+    private Integer nbrEtudiant;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public Integer getNbrEtudiant() {
+        return nbrEtudiant;
+    }
+
+    public void setNbrEtudiant(Integer nbrEtudiant) {
+        this.nbrEtudiant = nbrEtudiant;
+    }
+
+}
